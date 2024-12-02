28ff111539550e9de1233d468c90c9747783423c
lacsapadnan <pascaladnan101@gmail.com>
2024-12-02 14:35:09 +0700

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; {{ now()->year }} <div class="bullet"></div> Develop By <a href="https://ankhdigital.com">ANKH Digital</a>
+        Copyright &copy; {{ now()->year }} <div class="bullet"></div> Develop By <a href="https://pixeloverture.net">Pixeloverture</a>
     </div>
     <div class="footer-right">
-        2.3.0
+        3.3.0
     </div>
 </footer>
